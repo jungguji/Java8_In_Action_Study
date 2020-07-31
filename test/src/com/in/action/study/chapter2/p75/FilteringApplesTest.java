39cd1a68e262db7668bebe907c2de9dfa9439888
junggu <junggu.ji.dev@gmail.com>
2020-07-31 14:08:10 +0900

    type : Feat
    클래스 따로 생성하지 않고 익명 클래스로 동작 파라미터화

---
+++ test/src/com/in/action/study/chapter2/p75/FilteringApplesTest.java
@@ -0,0 +1,41 @@
+package com.in.action.study.chapter2.p75;
+
+import com.in.action.study.chapter2.Apple;
+import com.in.action.study.chapter2.ApplePredicate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FilteringApplesTest {
+
+    private List<Apple> inventory;
+    private FilteringApples filteringApples;
+
+    @BeforeEach
+    void setUp() {
+        this.filteringApples = new FilteringApples();
+
+        this.inventory = Arrays.asList(
+                new Apple(80,"green")
+                , new Apple(155, "green")
+                , new Apple(120, "red")
+        );
+    }
+
+    @Test
+    void filter() {
+        List<Apple> redApples = this.filteringApples.filter(inventory, new ApplePredicate() {
+            @Override
+            public boolean test(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        });
+
+        assertEquals(1, redApples.size());
+        assertEquals(120, redApples.get(0).getWeight());
+    }
+}
